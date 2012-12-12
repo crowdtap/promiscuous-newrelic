6e676e425c765cd38cfd52e61d1e6414c5a5e935
Nicolas Viennot <nicolas@viennot.biz>
2012-12-12 17:03:10 -0500

    Better names

--- lib/promiscuous/new_relic/instrumentation.rb
+++ lib/promiscuous/new_relic/instrumentation.rb
@@ -8,7 +8,7 @@ DependencyDetection.defer do
   end
 
   executes do
-    Promiscuous::Common::Worker.class_eval do
+    Promiscuous::Common::Worker.module_eval do
       include NewRelic::Agent::Instrumentation::ControllerInstrumentation
 
       alias_method :unit_of_work_without_rpm, :unit_of_work
@@ -16,10 +16,13 @@ DependencyDetection.defer do
         # XXX All the publishers are using the same category
         # (Just because it's hard to make sense of what it really means
         # to split things up. Maybe we should not even report the publisher worker.
-        worker_type = type == 'publisher' ? "Publishers" : "Subscribers"
+        worker_type = type == 'publisher' ? 'Publisher' : 'Subscriber'
+        name        = type == 'publisher' ? 'all' : type
 
-        perform_action_with_newrelic_trace(:class_name => type,
-                                           :category => "Promiscuous/#{worker_type}") do
+        # We are not using the subscriber class name, because of polymorphism
+        # We only want the parent class basically
+        perform_action_with_newrelic_trace(:name => name, :class_name => worker_type,
+                                           :category => "OtherTransaction/Promiscuous") do
           unit_of_work_without_rpm(type, &block)
         end
       end
